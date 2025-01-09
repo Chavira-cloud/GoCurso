1f072b9bc9eb388a287bfc7866055b985a1dd341
chavira-cloud <markos.mcgc@gmail.com>
2025-01-08 21:45:54 -0700

    creacion de pipelines

--- github-tracker/main.go
+++ github-tracker/main.go
@@ -21,6 +21,23 @@ func postHandler(w http.ResponseWriter, r *http.Request){
 
 }
 
+func insertGitHubWebhook(ctx context.Context, repo repository.Commit, webhook models.GitHubWebhook, body string, createdTime time.Time) error {
+	commit := entity.Commit{
+		RepoName:       webhook.Repository.FullName,
+		CommitID:       webhook.HeadCommit.ID,
+		CommitMessage:  webhook.HeadCommit.Message,
+		AuthorUsername: webhook.HeadCommit.Author.Username,
+		AuthorEmail:    webhook.HeadCommit.Author.Email,
+		Payload:        body,
+		CreatedAt:      createdTime,
+		UpdatedAt:      createdTime,
+	}
+
+	err := repo.Insert(ctx, &commit)
+
+	return err
+}
+
 func main() {
 	router := mux.NewRouter()
 
